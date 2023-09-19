9ccdd500d1b5f9a6854c28d0a15ecdaebf3cdca8
rajvanshyr <rajvanshyr@utexas.edu>
2023-09-18 22:18:56 -0700

    added homepage

--- Hello.py
+++ Hello.py
@@ -24,25 +24,18 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# Welcome to Raghav's Portfolio Site! 👋")
 
     st.sidebar.success("Select a demo above.")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
+        Raghav is a generative AI developer who is passionate about helping buisnesses implement use cases
         **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+        of what I've built
+        ### Want to get in touch?
+        - Reach out on Linkedin
+        - Check out my Medium
     """
     )
 
