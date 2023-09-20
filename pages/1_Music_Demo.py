f7bb7da2f9f63ca909700e2207fba4bea7f2b043
rajvanshyr <rajvanshyr@utexas.edu>
2023-09-20 08:35:37 -0700

    intial music

--- pages/1_Music_Demo.py
+++ pages/1_Music_Demo.py
@@ -3,6 +3,10 @@ import streamlit as st
 st.title("📝 Music Generation")
 
 txt = st.text_area('Text to analyze')
+st.button("Generate Music", type="primary")
 
+
+if st.button("Generate Music"):
+	st.write('Calling APi')
 #st.write('Sentiment:', run_sentiment_analysis(txt))
 
