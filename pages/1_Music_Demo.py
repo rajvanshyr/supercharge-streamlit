651cd7954662049ea16cd6fe655c8de96c50ac3f
rajvanshyr <rajvanshyr@utexas.edu>
2023-09-20 19:55:54 -0700

    api call 2

--- pages/1_Music_Demo.py
+++ pages/1_Music_Demo.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import openai
 
+with st.sidebar:
+    anthropic_api_key = st.text_input("Openai API Key", key="file_qa_api_key", type="password")
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
 st.title("📝 Music Generation")
 
 txt = st.text_area('Text to analyze')
