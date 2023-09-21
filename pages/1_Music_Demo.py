0d8fc5bb27ba56e2d4d1cd764df9b60672a77e0a
rajvanshyr <rajvanshyr@utexas.edu>
2023-09-20 19:37:07 -0700

    prompt

--- pages/1_Music_Demo.py
+++ pages/1_Music_Demo.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import openai
 
 st.title("📝 Music Generation")
 
@@ -9,5 +10,7 @@ txt = st.text_area('Text to analyze')
 if st.button("Generate Music"):
 	st.write('Calling APi with following txt')
 	st.write(txt)
+	prom= "Generate the next line for a rap song for reference this is the previous line "+txt
+	st.write(prom)
 #st.write('Sentiment:', run_sentiment_analysis(txt))
 
