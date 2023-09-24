c6e588a95dd907b818a2d4dc7346e2974a5ff0ec
rajvanshyr <rajvanshyr@utexas.edu>
2023-09-24 14:38:44 -0700

    staying

--- pages/1_Music_Demo.py
+++ pages/1_Music_Demo.py
@@ -60,7 +60,8 @@ if st.button("Generate Music"):
 	#response = st_text_rater(text=response["choices"][0]['text'])
 	#st.write(response)
 	#st.write(f"response --> {response}")
-
+	st.write('Rating:', values)
+	st.write('Staying')
 
 
 
