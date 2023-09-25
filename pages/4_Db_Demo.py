6b25dfb9c51fccedf835052af507ae0b475951a5
rajvanshyr <rajvanshyr@utexas.edu>
2023-09-24 17:54:40 -0700

    db 2

--- pages/4_Db_Demo.py
+++ pages/4_Db_Demo.py
@@ -1,35 +1,37 @@
 import streamlit as st
 import openai
+import sqlite3
+from sqlite3 import Error
 
 def create_connection(path):
-    connection = None
-    try:
-        connection = sqlite3.connect(path)
-        st.write("Connection to SQLite DB successful")
-    except Error as e:
-    	st.write('Below is error:', e, 'Above is a error.')
-        st.write("The error '{e}' occurred")
+	connection = None
+	try:
+		connection = sqlite3.connect(path)
+		st.write("Connection to SQLite DB successful")
+	except Error as e:
+		st.write('Below is error:', e, 'Above is a error.')
+		#st.write("The error '{e}' occurred")
 
-    return connection
+	return connection
 
 with st.sidebar:
-    anthropic_api_key = st.text_input("Openai API Key", key="file_qa_api_key", type="password")
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+	anthropic_api_key = st.text_input("Openai API Key", key="file_qa_api_key", type="password")
+	"[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
+	"[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("📝 Meeting Summarization")
+st.title("📝 DB Demo")
 
 connection = create_connection("E:\\sm_app.sqlite")
 
 
 def execute_query(connection, query):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        connection.commit()
-        print("Query executed successfully")
-    except Error as e:
-        print(f"The error '{e}' occurred")
+	cursor = connection.cursor()
+	try:
+		cursor.execute(query)
+		connection.commit()
+		print("Query executed successfully")
+	except Error as e:
+		print(f"The error '{e}' occurred")
 
 
 
