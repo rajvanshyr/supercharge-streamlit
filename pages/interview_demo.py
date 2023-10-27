d92e9af9dafcd4cf4e7e11d48eb0a189308ecb16
rajvanshyr <rajvanshyr@utexas.edu>
2023-10-27 10:19:10 -0700

    form 4

--- pages/interview_demo.py
+++ pages/interview_demo.py
@@ -1,12 +1,16 @@
 import streamlit as st
 
 form = st.form("my_form")
-title = form.text_input('Movie title', 'Life of Brian')
-form.slider("Inside the form")
+Company = form.text_input('Company', 'Google')
+Role = form.text_input('Position Interviwing For', 'Product manager')
+Hm = form.text_input('Position of Interviwer', 'Engineering Manager')
+
+
+form.slider("Level of Experience")
 st.slider("Outside the form")
 
 # Now add a submit button to the form:
 form.form_submit_button("Submit")
 
 
-st.write('The current movie title is', title)
+st.write('The current movie title is', Role)
