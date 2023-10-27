88da08b78ef28bb7094140d534a6b75f5227a180
rajvanshyr <rajvanshyr@utexas.edu>
2023-10-27 13:45:48 -0700

    prompt 2

--- pages/interview_demo.py
+++ pages/interview_demo.py
@@ -39,6 +39,6 @@ if open_api_key and x:
 	prom= f'I am interviewing as a {Role} position with the {Hm} at {Company}. \
 	What are the top 3 questions he is most likely to ask me? For reference this is a quick description of what they do {x}\
 	'
-	st.write('The prompt output is', p)
+	st.write('The prompt output is', prom)
 
 #st.write('The current movie title is', Role)
