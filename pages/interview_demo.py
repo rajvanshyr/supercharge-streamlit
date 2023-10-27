a71160f66335c04381f5fdd5509998ffe10e6946
rajvanshyr <rajvanshyr@utexas.edu>
2023-10-27 10:00:25 -0700

    form 2

--- pages/interview_demo.py
+++ pages/interview_demo.py
@@ -1,8 +1,12 @@
 import streamlit as st
 
 form = st.form("my_form")
+title = st.text_input('Movie title', 'Life of Brian')
 form.slider("Inside the form")
 st.slider("Outside the form")
 
 # Now add a submit button to the form:
 form.form_submit_button("Submit")
+
+
+st.write('The current movie title is', title)
