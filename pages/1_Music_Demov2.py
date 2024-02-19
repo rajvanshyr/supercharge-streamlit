415ff27c429c5471168448d6d4502b017f046b48
rajvanshyr <rajvanshyr@utexas.edu>
2024-02-19 14:29:30 -0800

    new reqs

--- pages/1_Music_Demov2.py
+++ pages/1_Music_Demov2.py
@@ -18,7 +18,7 @@ with st.sidebar:
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("📝 Music Generation")
+st.title("📝 Rap Pad")
 
 st.markdown(
         """
